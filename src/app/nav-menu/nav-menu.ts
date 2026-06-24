63c6a2d5f2de52113962c7ce4d91568e7cca4761
Martiniano Gutierrez <martu.gutierrez82@gmail.com>
2026-06-24 17:42:19 -0300

    Cambio colores a menos brillantes, arreglo carrito del nav y cambio texto del footer

--- src/app/nav-menu/nav-menu.ts
+++ src/app/nav-menu/nav-menu.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { YerbaCartService } from '../service-yerba-cart/yerba-cart-service';
+import { Yerba } from '../yerba-list/Yerba';
 
 @Component({
   selector: 'app-nav-menu',
@@ -7,4 +9,15 @@ import { Component } from '@angular/core';
   templateUrl: './nav-menu.html',
   styleUrl: './nav-menu.scss',
 })
-export class NavMenu {}
+export class NavMenu {
+
+    yerbaCartList: Yerba[] = [];
+    constructor(private cartService: YerbaCartService) {
+      cartService.cartlist.subscribe(cartItem => this.yerbaCartList = cartItem);
+    }
+  
+    getTotalQuantity() {
+      return this.yerbaCartList.reduce((total, item) => total + item.quantity, 0);
+    }
+
+}
