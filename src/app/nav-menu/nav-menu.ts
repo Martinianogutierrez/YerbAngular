ce44a7924a8f086a1bda0b04ac39915bcbdcff95
Martiniano <martu.gutierrez82@gmail.com>
2026-08-15 12:21:47 -0300

    Implement routerLink for navigation in the nav menu

--- src/app/nav-menu/nav-menu.ts
+++ src/app/nav-menu/nav-menu.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { YerbaCartService } from '../service-yerba-cart/yerba-cart-service';
 import { Yerba } from '../yerba-list/Yerba';
 
 @Component({
   selector: 'app-nav-menu',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './nav-menu.html',
   styleUrl: './nav-menu.scss',
 })
